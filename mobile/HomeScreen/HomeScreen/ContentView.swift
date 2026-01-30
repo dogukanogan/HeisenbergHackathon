1b0f7863709318217d532ba8ed67dfa582d5ba4b
gorkemclk <grkmclk10@gmail.com>
2026-01-30 18:17:24 +0300

    homescreen project

---
+++ mobile/HomeScreen/HomeScreen/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HomeScreen
+//
+//  Created by Görkem Çelik on 30.01.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
